cf397ae68ba3b046f3e4da3063bb2b646328f28f
Agustin Tuduri Marin <agustin.tuduri@w2m.com>
2024-03-20 14:32:01 +0100

    10- Event Binding

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -14,6 +14,9 @@ export class EmpleadoComponent implements OnInit {
   empresa: String = "W2M"
   habilitado = false;  // ESTE VALOR DETERMINARÁ EL VALOR DE LA PROPIEDAD DEL ELEMENTO HTML
   usuRegistrado = false; // ESTE VALOR DETERMINARÁ EL VALOR DE LA PROPIEDAD DEL ELEMENTO HTML
+  textoDeRegistro = "No hay nadie registrado";
+  trabajando = true;
+  textoLaboral = "";
 
   getEdad() {
     return this.edad;
@@ -24,7 +27,27 @@ export class EmpleadoComponent implements OnInit {
   }
 
   getRegistroUsuario() {
-    this.usuRegistrado = true;
+    this.usuRegistrado = false;
+  }
+
+  mostrarVentana() {
+    alert("Usuario Registrado")
+  }
+
+  cambiaTexto() {
+    this.textoDeRegistro = "El usuario está ahora registrado"
+  }
+
+  setTextoLaboral(event: Event) {
+    if ((<HTMLInputElement>event.target).value == "paro") {  // SE DEBE CASTEAR EL TARGET DEL EVENTO AL TIPO DE ELEMENTO HTML EN ESTE CASO HTMLInputElement
+      this.textoLaboral = "El trabajador está en paro";     //EN EL VÍDEO LO COMPRUEBA HACIENDO alert(event.target)
+    }
+
+    else {
+      this.textoLaboral = "El trabajador está trabajando";
+    }
+
+
   }
 
 
